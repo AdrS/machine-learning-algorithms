6b2dd607668608343840ce992edc557827388cf3
Adrian Stoll <adrs@umich.edu>
2023-11-23 11:27:13 -0800

    Generate HTML report with distributions of fields

--- data_exploration.py
+++ data_exploration.py
@@ -1,8 +1,15 @@
 import argparse
+import io
 import math
+import matplotlib.pyplot as plt
 import numpy as np
+import os
 import pandas as pd
+import seaborn as sns
+import shutil
+import sys
 from data_transform import pick_smallest_datatypes
+from xml.etree import ElementTree as ET
 
 class FieldSummary:
 
@@ -18,9 +25,7 @@ class FieldSummary:
                 ascending=False)/len(series)
             self.description['entropy'] = \
               -self.most_frequent.map(lambda x: x*math.log2(x)).sum()
-
-def is_numerical(series):
-    return series.dtype == float or series.dtype == int or series.dtype == bool
+        self.series = series
 
 def set_categorical_fields(X, max_categorical_int_unique_values=20):
     for field in X.columns:
@@ -29,24 +34,92 @@ def set_categorical_fields(X, max_categorical_int_unique_values=20):
 
 class SummaryReporter:
 
+    def __init__(self, output_directory):
+        self.output_directory = output_directory
+        self.num_figures = 0
+
     def report_dataframe(self, df, max_rows=None):
         raise NotImplementedError
 
     def report_heading(self, heading, level=None):
         raise NotImplementedError
 
-class StdoutReporter:
+    def save_figure(self, name=None):
+        if not name:
+            name = 'fig%d.png' % (self.num_figures,)
+            self.num_figures += 1
+        plt.savefig(os.path.join(self.output_directory, name))
+        plt.close()
+        return name
+
+    def print(self, *args):
+        raise NotImplementedError
+
+    def finish(self):
+        raise NotImplementedError
+
+class PlainTextReporter(SummaryReporter):
+
+    def __init__(self, output_directory):
+        super().__init__(output_directory)
+        self.fd = open(os.path.join(output_directory, 'report.txt'), 'w')
 
     def report_dataframe(self, df, max_rows=None):
-        print(df.to_string())
+        self.print(df.to_string())
 
     def report_heading(self, heading, level=None):
         if level:
             # Number of blank lines between sections is dependent on the
             # heading level.
             for _ in range(max(0, 4 - level)):
-                print('')
-        print(heading + ':')
+                self.print('')
+        self.print(heading + ':')
+
+    def save_figure(self, name=None):
+        path = super().save_figure(name)
+        self.print('Figure:', path)
+
+    def print(self, *args):
+        print(*args, file=self.fd)
+
+    def finish(self):
+        self.fd.close()
+
+class HtmlReporter(SummaryReporter):
+
+    def __init__(self, output_directory):
+        super().__init__(output_directory)
+        self.body = ET.Element('body')
+
+    def report_dataframe(self, df, max_rows=None):
+        pre = ET.Element('pre')
+        pre.text = df.to_string()
+        self.body.append(pre)
+
+    def report_heading(self, heading, level=None):
+        h = ET.Element('h%d' % (level,))
+        h.text = heading
+        self.body.append(h)
+
+    def save_figure(self, name=None):
+        path = super().save_figure(name)
+        img = ET.Element('img', src=path)
+        self.body.append(img)
+
+    def print(self, *args):
+        div = ET.Element('div')
+        output = io.StringIO()
+        print(*args, file=output)
+        div.text = output.getvalue()
+        output.close()
+        self.body.append(div)
+
+    def finish(self):
+        html = ET.Element('html')
+        html.append(self.body)
+        path = os.path.join(self.output_directory, 'report.html')
+        with open(path, 'wb') as f:
+            ET.ElementTree(html).write(f, encoding='utf-8', method='html')
 
 class CategoricalFieldSummary(FieldSummary):
 
@@ -76,6 +149,10 @@ class NumericalFieldSummary(FieldSummary):
         if 'entropy' in self.description:
             fields += ['entropy']
         reporter.report_dataframe(self.description[fields])
+
+        sns.displot(self.series)
+        reporter.save_figure()
+
         if self.most_frequent is not None:
             reporter.report_heading('Most frequent values', 3)
             reporter.report_dataframe(self.most_frequent)
@@ -105,6 +182,20 @@ class DatasetSummary:
             reporter.report_heading(f'Field {field}', 2)
             summary.report(reporter)
 
+        reporter.report_heading('Less useful fields', 2)
+        reporter.print('Mostly missing fields:', self.mostly_missing_fields())
+        reporter.print('Low entropy fields:', self.low_entropy_fields())
+        reporter.finish()
+
+    def mostly_missing_fields(self, threshold=0.9):
+        return [field for field, summary in self.fields.items()
+            if summary.description['percent_missing'] > threshold]
+
+    def low_entropy_fields(self, threshold=0.07638839439271368):
+        return [field for field, summary in self.fields.items()
+            if 'entropy' in summary.description and
+            summary.description['entropy'] < threshold]
+
 def load_dataset(path, missing_header, optimize_memory=False):
     kwargs = {}
     if missing_header:
@@ -123,12 +214,23 @@ def load_dataset(path, missing_header, optimize_memory=False):
     return dataset
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--dataset', required=True)
-    parser.add_argument('--missing_header', action='store_true')
+    parser = argparse.ArgumentParser(
+        description='Generates a report describing the features of the dataset'
+    )
+    parser.add_argument('--dataset', required=True,
+        help='Path to a CSV file containing the dataset')
+    parser.add_argument('--missing_header', action='store_true',
+        help='Whether the first line of the CSV file is the header')
     parser.add_argument('--optimize_memory', action='store_true')
+
+    parser.add_argument('--output_directory', required=True,
+        help='Directory to save the report to')
+    parser.add_argument('--wipe_existing_output', action='store_true',
+        help='Whether to delete any existing files in the output directory')
     args = parser.parse_args()
+    if args.wipe_existing_output:
+        shutil.rmtree(args.output_directory)
+    os.makedirs(args.output_directory, exist_ok=True)
     dataset = load_dataset(args.dataset, args.missing_header,
         args.optimize_memory)
-
-    DatasetSummary(dataset).report(StdoutReporter())
+    DatasetSummary(dataset).report(HtmlReporter(args.output_directory))
