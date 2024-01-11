fcbbefee010fe6af0dc65a4d7ccac3b81d7e78fb
Adrian Stoll <adrs@umich.edu>
2024-01-10 22:03:48 -0800

    Refactor preprocessing pipeline and compute feature importances
    - Refactor the data preprocessing steps to use sklearn components
    - Evaluate the importance of each feature using permutation importance

--- playground_series_s4e1_model.py
+++ playground_series_s4e1_model.py
@@ -2,49 +2,24 @@ import numpy as np
 import pandas as pd
 from collections import defaultdict
 from functools import reduce
+from sklearn.compose import ColumnTransformer
 from sklearn.ensemble import AdaBoostClassifier, GradientBoostingClassifier, RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import roc_auc_score
 from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPClassifier
+from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
 from sklearn.svm import LinearSVC, SVC
+from sklearn.inspection import permutation_importance
 
-class SparseToDense:
-    def transform(self, X):
-        return X.toarray()
-
-def fit_transform(X,
-    dropped_features=[],
-    numerical_features=[],
-    categorical_features=[]):
-    # Use sklearn pipeline instead?
-    transforms = defaultdict(list)
-    for feature in numerical_features:
-        scaler = StandardScaler()
-        scaler.fit(X[[feature]])
-        transforms[feature].append(scaler)
-
-    # TODO: add other transformation like log, sqrt, square
-    # TODO: make which transforms to apply a hyper-parameter
-
-    for feature in categorical_features + binary_features:
-        encoder = OneHotEncoder(drop='if_binary')
-        encoder.fit(X[[feature]])
-        transforms[feature].append(encoder)
-        transforms[feature].append(SparseToDense())
-
-    def transform(X):
-        X = X.drop(columns=dropped_features)
-        X_parts = []
-        for feature in X.columns:
-            X_parts.append(
-                reduce(lambda x, t: t.transform(x),
-                    transforms[feature], X[[feature]]))
-        return np.hstack(X_parts)
-    return transform
-
-# TODO: feature engineering transform to combine features
+def most_important_features(model, X_val, Y_val, preprocess):
+    pipeline = Pipeline([('preprocess', preprocess), ('model', model)])
+    r = permutation_importance(pipeline, X_val, Y_val, scoring='roc_auc')
+    return pd.DataFrame({
+        'mean':r.importances_mean,
+        'std':r.importances_std},
+        index=X_val.columns).sort_values(by=['mean'], ascending=False)
 
 if __name__ == '__main__':
     # TODO: refactor into a library to use for other datasets
@@ -57,15 +32,21 @@ if __name__ == '__main__':
     binary_features = ['HasCrCard', 'IsActiveMember'] 
     categorical_features = ['Geography', 'Gender']
 
+    preprocess = ColumnTransformer([
+        ('Categorical', OneHotEncoder(drop='if_binary'),
+            categorical_features + binary_features),
+        ('Numerical', StandardScaler(), numerical_features)
+    ])
+
     train_dataset = pd.read_csv(train_path)
     Y = train_dataset[target]
-    X = train_dataset.drop(columns=[target])
+    X = train_dataset.drop(columns=[target] + dropped_features)
 
-    X_train, X_val, Y_train, Y_val = train_test_split(X, Y, test_size=0.2, random_state=seed)
-    transform = fit_transform(X_train, dropped_features, numerical_features, categorical_features)
-
-    X_train = transform(X_train)
-    X_val = transform(X_val)
+    X_train, X_val, Y_train, Y_val = train_test_split(X, Y, test_size=0.2,
+                                        random_state=seed)
+    # Cache the preprocessed training data
+    X_train_post_processed = preprocess.fit_transform(X_train)
+    X_val_post_processed = preprocess.transform(X_val)
 
     # Try different families of models as a first pass
     classifiers = [
@@ -74,15 +55,17 @@ if __name__ == '__main__':
         AdaBoostClassifier(),
         RandomForestClassifier(),
         GradientBoostingClassifier(),
-        LinearSVC(),
         SVC(),
     ]
     for classifier in classifiers:
         print('Fitting classifier:', classifier)
-        classifier.fit(X_train, Y_train)
+        classifier.fit(X_train_post_processed, Y_train)
 
-        predictions = classifier.predict_proba(X_val)
+        predictions = classifier.predict_proba(X_val_post_processed)
         score = roc_auc_score(Y_val, predictions[:, 1])
-        print('Score:', score)
+        print('Score (roc_auc):', score)
+        print('Most important features (permutation importance):')
+        print(most_important_features(classifier, X_val, Y_val, preprocess))
+        print('\n')
 
     # Select the best and run hyper parameter searches
