41103becbe066d42ce414973b16a9f8f56948517
Adrian Stoll <adrs@umich.edu>
2024-01-23 21:02:05 -0800

    Print roc auc and confusion matrix for train and validation sets

--- playground_series_s4e1_model.py
+++ playground_series_s4e1_model.py
@@ -5,7 +5,7 @@ from functools import reduce
 from sklearn.compose import ColumnTransformer
 from sklearn.ensemble import AdaBoostClassifier, GradientBoostingClassifier, RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import roc_auc_score
+from sklearn.metrics import roc_auc_score, confusion_matrix
 from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPClassifier
 from sklearn.pipeline import Pipeline
@@ -61,6 +61,19 @@ def most_important_features(model, X_val, Y_val, **kwargs):
         'mean':r.importances_mean,
         'std':r.importances_std},
         index=X_val.columns).sort_values(by=['mean'], ascending=False)
+
+def print_evaluation(model, X, Y):
+    prediction_proba = model_pipeline.predict_proba(X)
+    # TODO: use get_scorer
+    score = roc_auc_score(Y, prediction_proba[:, 1])
+    print('Score (roc_auc):', score)
+    threshold = 0.5
+    predictions = prediction_proba[:, 1] >= threshold
+    print(f'Confusion Matrix (threshold = {threshold}):')
+    print(pd.DataFrame(
+            confusion_matrix(Y, predictions, normalize='all'),
+            index=[f'Actual: {i}' for i in [False, True]],
+            columns=[f'Predicted: {i}' for i in [False, True]]))
 
 if __name__ == '__main__':
     # TODO: refactor into a library to use for other datasets
@@ -122,18 +135,20 @@ if __name__ == '__main__':
             ('Column transform', column_transformer),
             ('Model', model)
         ])
-        print('Evaluating model family:', model)
+        print('\n\nEvaluating model family:', model)
         # TODO: feature selection
         X_train_features = X_train_all_features
         X_val_features = X_val_all_features
 
         model_pipeline.fit(X_train_features, Y_train)
 
-        predictions = model_pipeline.predict_proba(X_val_features)
-        # TODO: use get_scorer
-        score = roc_auc_score(Y_val, predictions[:, 1])
-        print('Score (roc_auc):', score)
-        print('Most important features (permutation importance):')
+        print('Training scores:')
+        print_evaluation(model_pipeline, X_train_features, Y_train)
+
+        print('\nValidation scores:')
+        print_evaluation(model_pipeline, X_val_features, Y_val)
+
+        print('\nMost important features (permutation importance):')
         print(most_important_features(model_pipeline, X_val_features, Y_val,
             scoring=scoring))
 
